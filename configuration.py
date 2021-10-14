f33333750445b2d4baab6907d2c806a9ee5d2fbd
Marcos Amorim <mamorim@redhat.com>
2021-10-14 09:05:34 -0400

    Fix default tag typo

--- configuration.py
+++ configuration.py
@@ -4,4 +4,4 @@ with open('configuration.toml') as f:
     config = toml.load(f)
 
 discovery_hostname = socket.gethostname()
-discovery_tag = f"Autodiscovered-{discovery_hostname}"
+discovery_tag = f"Auto Discovered by host {discovery_hostname}"
